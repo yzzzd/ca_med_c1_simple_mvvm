afe72501084f69edb7c8c7fe7882d38c5fc50cb4
Zidd <nuvyazid@gmail.com>
2021-10-08 13:16:29 +0700

    implement mvvm

--- app/src/main/java/id/nuryaz/counter/MainActivity.kt
+++ app/src/main/java/id/nuryaz/counter/MainActivity.kt
@@ -2,35 +2,19 @@ package id.nuryaz.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import id.nuryaz.counter.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    private var counter = 0
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-
-    private fun increaseCounter() {
-        counter += 1
-        binding.tvCounter.text = "$counter"
-    }
-
-    private fun decreaseCounter() {
-        counter -= 1
-        binding.tvCounter.text = "$counter"
-    }
-
-    override fun onClick(p0: View?) {
-        when (p0) {
-            binding.btnPlus -> increaseCounter()
-            binding.btnMinus -> decreaseCounter()
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
     }
 }
